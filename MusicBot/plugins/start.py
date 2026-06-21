e53d486d7e9b3eddc8e45d6704767dcd164203eb
khansohail5765 <khansohail5765@gmail.com>
2026-06-21 10:15:15 +0530

    Update start.py

--- MusicBot/plugins/start.py
+++ MusicBot/plugins/start.py
@@ -1,1 +1,7 @@
-# Start Command
+from pyrogram import filters
+from pyrogram.types import Message
+from MusicBot.bot import app
+
+@app.on_message(filters.command("start"))
+async def start_command(client, message: Message):
+    await message.reply_text("🎵 Music Bot Online")
